b02b88f153b38d6f347078fb6e4a7b1cfc3aafdb
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-06-08 19:14:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.py
@@ -0,0 +1,12 @@
+class Solution:
+    def floydWarshall(self, dist):
+        n = len(dist)
+        INF = 100000000
+        
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if dist[i][k] != INF and dist[k][j] != INF:
+                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+        
+        return dist
